89a8eb7fee22ebd95488189d7efbe1442c5a7d64
Brian Khoo <102838003+bkhoo123@users.noreply.github.com>
2023-02-19 08:12:36 +0000

    sequelize fixed

--- backend/db/migrations/20230215011511-create-subscription-plan.js
+++ backend/db/migrations/20230215011511-create-subscription-plan.js
@@ -1,7 +1,10 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 
-
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -30,9 +33,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
+    options.tableName = 'SubscriptionPlans'
     await queryInterface.dropTable('SubscriptionPlans');
   }
 };
